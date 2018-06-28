dde03c281cd1bb210c6fb18161022a54952a1f04
Bibek Shrestha <bbekstha96@gmail.com>
2018-06-28 12:42:49 -0700

    Making call to the api

--- sourceProtected.js
+++ sourceProtected.js
@@ -23,30 +23,60 @@ console.log("GET AND INSERT PETS")
       "Content-Type": "application/json"
    } 
 
-   let auth_token_local = "eyJraWQiOiJTNlp6cWFZdzh2SlFcLyszUXRoUldnRGp6M0srTWFvOElTZWxST0RPSmh3TT0iLCJhbGciOiJSUzI1NiJ9.eyJhdF9oYXNoIjoieGdyY2RBZkpZOFN6d1hwcEZqNHc5QSIsInN1YiI6IjBkZTA5NTZmLTE3ZjItNDE3OC05YzU0LTExODljNThmOTUyZCIsImF1ZCI6IjJmaW9yNjc3MGh2dG80dTZrdXEwODRqN2Z1IiwiZXZlbnRfaWQiOiJjMjg4OTQ3OC03YWZmLTExZTgtYTJiMC1iNTI0ZDkyNzIzNGQiLCJ0b2tlbl91c2UiOiJpZCIsImF1dGhfdGltZSI6MTUzMDIwOTk1MywiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLnVzLXdlc3QtMi5hbWF6b25hd3MuY29tXC91cy13ZXN0LTJfTGxvZFlneVFOIiwiY29nbml0bzp1c2VybmFtZSI6ImJzaHJlc3RoIiwiZXhwIjoxNTMwMjEzNTUzLCJpYXQiOjE1MzAyMDk5NTMsImVtYWlsIjoiYnNocmVzdGhAY2FscG9seS5lZHUifQ.K2F1gGB0MpLN93HZ0xczlK7X4GJTSkiMi854hcC8_w64vTZVYi8sHfrXQ1RIy5PFo0d0vTKnRv9-vYo1Etu2EYGWuv91EjeqPvPNrt0f_a80i6d7JuV1c9WFHUYhPYbfQYMr9GIBR1gZh_oyW4CIE5jMDpx554dLY3U-46ufQ1vrIMlvv8i8n1MNrbpXmt40J1RqRzaoNyxQp6CosfYFWOjr4XhxpO9UD9ospvMRddcvoeucRIcKRgvDGqIkK6NOhVtHuDDp1lDV9c8O520-SDROvXSk9KgQzIBiQ6gAMd4xfGERgdSsIM_gj9Z0yXcPxslQOfeLkNCzvZqQ08RsHw"
+   // let auth_token_local = "eyJraWQiOiJTNlp6cWFZdzh2SlFcLyszUXRoUldnRGp6M0srTWFv
+   // OElTZWxST0RPSmh3TT0iLCJhbGciOiJSUzI1NiJ9.eyJhdF9oYXNoIjoieGdyY2RBZkpZ
+   // OFN6d1hwcEZqNHc5QSIsInN1YiI6IjBkZTA5NTZmLTE3ZjItNDE3OC05YzU0LTExODl
+   // jNThmOTUyZCIsImF1ZCI6IjJmaW9yNjc3MGh2dG80dTZrdXEwODRqN2Z1IiwiZXZlbnRfaWQi
+   // OiJjMjg4OTQ3OC03YWZmLTExZTgtYTJiMC1iNTI0ZDkyNzIzNGQiLCJ0b2tlbl91c2UiOiJpZCIsI
+   // mF1dGhfdGltZSI6MTUzMDIwOTk1MywiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLnVz
+   // LXdlc3QtMi5hbWF6b25hd3MuY29tXC91cy13ZXN0LTJfTGxvZFlneVFOIiwiY29nbml0bzp1c2V
+   // ybmFtZSI6ImJzaHJlc3RoIiwiZXhwIjoxNTMwMjEzNTUzLCJpYXQiOjE1MzAyMDk5
+   // NTMsImVtYWlsIjoiYnNocmVzdGhAY2FscG9seS5lZHUifQ.K2F1gGB0MpLN93
+   // HZ0xczlK7X4GJTSkiMi854hcC8_w64vTZVYi8sHfrXQ1RIy5PFo0d0vTKnRv9-
+   // vYo1Etu2EYGWuv91EjeqPvPNrt0f_a80i6d7JuV1c9WFHUYhPYbfQYMr9GIBR1gZh_oyW4CIE
+   // 5jMDpx554dLY3U-46ufQ1vrIMlvv8i8n1MNrbpXmt40J1RqRzaoNyxQp6CosfYFWOjr4Xhxp
+   // O9UD9ospvMRddcvoeucRIcKRgvDGqIkK6NOhVtHuDDp1lDV9c8O520-SDROvXSk9KgQzIBiQ6gA
+   // Md4xfGERgdSsIM_gj9Z0yXcPxslQOfeLkNCzvZqQ08RsHw"
    let auth_token = getCookie("id_token")
    if (auth_token) {
       headers["Authorization"] = `Bearer ${auth_token}`;
    }
 
-   fetch(url, {headers, mode:'cors'}).then(function(response) {
-      response.json().then(function(petsJson) {
-console.log("MY PETS", petsJson)
-         var petKeys = Object.keys(petsJson);
+   var xhr = new XMLHttpRequest();
+   xhr.withCredentials = true;
 
-         for (var i = 0; i < petKeys.length; i++) {
-            var r = dispTblPet.createTHead().insertRow(0);
-            r.insertCell(i).innerHTML = key[i]
-         }
+   xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === 4) {
+         console.log(this.responseText);
+      }
+   });
 
-         for(pet in petsJson) {
-            var row = dispTblPet.insertRow();
-            for (var i = 0; i < petKeys.length; i++) {
-               row.insertCell(i).innerHTML = pet.petKeys[i];
-            }
-         }
-      })
-   })
+   xhr.open("GET", url);
+   xhr.setRequestHeader("Authorization", `Bearer ${auth_token}`);
+   xhr.setRequestHeader("Cache-Control", "no-cache");
+//    fetch(url, {headers, mode:'cors'}).then(function(response) {
+//       response.json().then(function(petsJson) {
+// console.log("MY PETS", petsJson)
+//          var petKeys = Object.keys(petsJson);
+
+//          for (var i = 0; i < petKeys.length; i++) {
+//             var r = dispTblPet.createTHead().insertRow(0);
+//             r.insertCell(i).innerHTML = key[i]
+//          }
+
+//          for(pet in petsJson) {
+//             var row = dispTblPet.insertRow();
+//             for (var i = 0; i < petKeys.length; i++) {
+//                row.insertCell(i).innerHTML = pet.petKeys[i];
+//             }
+//          }
+//       })
+//    })
+   xhr.onload = function() {
+      console.log(xhr.response)
+   }
+   
+   xhr.send()
 }
 
 function modifyContainer(startNode) {
